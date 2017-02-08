726afc0e0652588e9b74fafa0ede8a0924372567
Lenz Gschwendtner <lenz@springtimesoft.com>
2017-02-08 04:46:03 +0100

    leave this bit so we can define view modules as strings

--- lib/couch_view_manager.ex
+++ lib/couch_view_manager.ex
@@ -4,6 +4,8 @@ defmodule CouchViewManager do
 
   def migrate do
     Application.get_env(:couch_view_manager, :views)
+        |> Enum.map(&(String.capitalize(&1)))
+        |> Enum.map(fn(x) -> Module.concat(Views, x) end)
         |> Enum.map(&(%{&1 => apply(&1, :__info__, [:functions])}))
         |> Enum.map(&(check(&1)))
   end
